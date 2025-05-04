586b31e409a314ccae82f370c61c29714b1deea8
Jason Hall <jasonbrianhall@gmail.com>
2025-05-04 00:58:50 -0400

    Latest

---
+++ CMakeFiles/smbc.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jbhall/software_build/SuperMarioBros-C/source/Configuration.cpp" "CMakeFiles/smbc.dir/source/Configuration.cpp.o" "gcc" "CMakeFiles/smbc.dir/source/Configuration.cpp.o.d"
+  "/home/jbhall/software_build/SuperMarioBros-C/source/Emulation/APU.cpp" "CMakeFiles/smbc.dir/source/Emulation/APU.cpp.o" "gcc" "CMakeFiles/smbc.dir/source/Emulation/APU.cpp.o.d"
+  "/home/jbhall/software_build/SuperMarioBros-C/source/Emulation/Controller.cpp" "CMakeFiles/smbc.dir/source/Emulation/Controller.cpp.o" "gcc" "CMakeFiles/smbc.dir/source/Emulation/Controller.cpp.o.d"
+  "/home/jbhall/software_build/SuperMarioBros-C/source/Emulation/MemoryAccess.cpp" "CMakeFiles/smbc.dir/source/Emulation/MemoryAccess.cpp.o" "gcc" "CMakeFiles/smbc.dir/source/Emulation/MemoryAccess.cpp.o.d"
+  "/home/jbhall/software_build/SuperMarioBros-C/source/Emulation/PPU.cpp" "CMakeFiles/smbc.dir/source/Emulation/PPU.cpp.o" "gcc" "CMakeFiles/smbc.dir/source/Emulation/PPU.cpp.o.d"
+  "/home/jbhall/software_build/SuperMarioBros-C/source/Main.cpp" "CMakeFiles/smbc.dir/source/Main.cpp.o" "gcc" "CMakeFiles/smbc.dir/source/Main.cpp.o.d"
+  "/home/jbhall/software_build/SuperMarioBros-C/source/SMB/SMB.cpp" "CMakeFiles/smbc.dir/source/SMB/SMB.cpp.o" "gcc" "CMakeFiles/smbc.dir/source/SMB/SMB.cpp.o.d"
+  "/home/jbhall/software_build/SuperMarioBros-C/source/SMB/SMBData.cpp" "CMakeFiles/smbc.dir/source/SMB/SMBData.cpp.o" "gcc" "CMakeFiles/smbc.dir/source/SMB/SMBData.cpp.o.d"
+  "/home/jbhall/software_build/SuperMarioBros-C/source/SMB/SMBEngine.cpp" "CMakeFiles/smbc.dir/source/SMB/SMBEngine.cpp.o" "gcc" "CMakeFiles/smbc.dir/source/SMB/SMBEngine.cpp.o.d"
+  "/home/jbhall/software_build/SuperMarioBros-C/source/Util/Video.cpp" "CMakeFiles/smbc.dir/source/Util/Video.cpp.o" "gcc" "CMakeFiles/smbc.dir/source/Util/Video.cpp.o.d"
+  "" "smbc" "gcc" "CMakeFiles/smbc.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/smbc.dir/source/Emulation/MemoryAccess.cpp.o.d
@@ -0,0 +1,24 @@
+CMakeFiles/smbc.dir/source/Emulation/MemoryAccess.cpp.o: \
+ /home/jbhall/software_build/SuperMarioBros-C/source/Emulation/MemoryAccess.cpp \
+ /usr/include/stdc-predef.h \
+ /home/jbhall/software_build/SuperMarioBros-C/source/Emulation/../SMB/SMBEngine.hpp \
+ /usr/include/c++/14/cstdint \
+ /usr/include/c++/14/x86_64-redhat-linux/bits/c++config.h \
+ /usr/include/bits/wordsize.h \
+ /usr/include/c++/14/x86_64-redhat-linux/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/timesize.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/long-double.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-64.h \
+ /usr/include/c++/14/x86_64-redhat-linux/bits/cpu_defines.h \
+ /usr/lib/gcc/x86_64-redhat-linux/14/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/libc-header-start.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/time64.h /usr/include/bits/wchar.h \
+ /usr/include/bits/stdint-intn.h /usr/include/bits/stdint-uintn.h \
+ /usr/include/bits/stdint-least.h /usr/include/c++/14/cstddef \
+ /usr/lib/gcc/x86_64-redhat-linux/14/include/stddef.h \
+ /usr/include/c++/14/bits/version.h \
+ /home/jbhall/software_build/SuperMarioBros-C/source/Emulation/../SMB/../Emulation/MemoryAccess.hpp \
+ /home/jbhall/software_build/SuperMarioBros-C/source/Emulation/../SMB/SMBDataPointers.hpp \
+ /home/jbhall/software_build/SuperMarioBros-C/source/Emulation/MemoryAccess.hpp
