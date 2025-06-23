a5a48c5db730743ab61ef7d82278fda944ac176c
Jason Hall <jasonbrianhall@gmail.com>
2025-06-23 14:17:19 -0400

    Latest

---
+++ docs/createchr.sh
@@ -0,0 +1,1 @@
+dd if=~/nes/Super\ Mario\ Bros.\ \(JU\)\ \(PRG0\)\ \[\!].nes of=chr.bin bs=1 skip=32784 count=8192
---
+++ docs/createheader.sh
@@ -0,0 +1,1 @@
+printf "4E 45 53 1A 02 01 01 00 00 00 00 00 00 00 00 00" | xxd -r -p > header.bin
